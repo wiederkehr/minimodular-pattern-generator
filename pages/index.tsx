38006e4a3935af592f8040874bcd18b721eb1861
Benjamin Wiederkehr <benjamin@interactivethings.com>
2020-07-12 17:59:51 -0400

    Configurator #wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,32 @@ import AppMain from "../components/AppMain";
 import Configurator from "../components/Configurator";
 import Canvas from "../components/Canvas";
 
-export default function app() {
-  return (
-    <React.Fragment>
-      <AppSidebar>
-        <Configurator />
-      </AppSidebar>
-      <AppMain>
-        <Canvas />
-      </AppMain>
-    </React.Fragment>
-  );
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      construction: {},
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange = (construction) => {
+    this.setState(construction);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <AppSidebar>
+          <Configurator
+            construction={this.state.construction}
+            onChange={this.onChange}
+          />
+        </AppSidebar>
+        <AppMain>
+          <Canvas construction={this.state.construction} />
+        </AppMain>
+      </React.Fragment>
+    );
+  }
 }

3a51dbc2b545b5fd5e598ca7f5e7bd1b503c0be1
Benjamin Wiederkehr <benjamin@interactivethings.com>
2020-07-16 09:22:30 -0400

    Anonymize credits

--- components/Credits.tsx
+++ components/Credits.tsx
@@ -2,21 +2,7 @@
 import { jsx } from "theme-ui";
 
 const Credits: React.FC = () => (
-  <p sx={{ padding: 0, margin: 0 }}>
-    Built by{" "}
-    <a
-      sx={{
-        color: "link",
-        textDecoration: "none",
-        "&:hover": {
-          textDecoration: "underline",
-        },
-      }}
-      href="https://benjaminwiederkehr.com"
-    >
-      Benjamin Wiederkehr
-    </a>
-  </p>
+  <p sx={{ padding: 0, margin: 0 }}>Version 0.1</p>
 );
 
 export default Credits;

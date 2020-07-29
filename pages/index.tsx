094da939e9a49a1c9b66c48f40ec067771d6cc7c
Benjamin Wiederkehr <benjamin@interactivethings.com>
2020-07-29 07:42:46 -0400

    Replace two radio groups with single select

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import downloadSvg from "svg-crowbar";
 import ContainerDimensions from "react-container-dimensions";
-import { jsx, Flex, Box, Button, Label, Slider, Radio } from "theme-ui";
+import { jsx, Flex, Box, Button, Label, Slider, Radio, Select } from "theme-ui";
 import MainSidebar from "../components/MainSidebar";
 import MainContent from "../components/MainContent";
 import Canvas from "../components/Canvas";
@@ -46,6 +46,16 @@ export default class Index extends React.Component<Props, State> {
     const target: HTMLInputElement = event.currentTarget as HTMLInputElement;
     const value: string = target.value;
     this.setState(({ [target.name]: value } as unknown) as State);
+  };
+
+  handleSelectChange = (event: React.ChangeEvent) => {
+    const target: HTMLInputElement = event.currentTarget as HTMLInputElement;
+    const value: string = target.value;
+    const values: Array = value.split(",");
+    this.setState(({
+      closure: values[0],
+      fold: values[1],
+    } as unknown) as State);
   };
 
   handleSubmit = (event: React.FormEvent) => {
@@ -107,101 +117,73 @@ export default class Index extends React.Component<Props, State> {
         </MainContent>
         <MainSidebar>
           <Box as="form" onSubmit={this.handleSubmit}>
-            <Label>Closure</Label>
-            <Flex mb={3}>
-              <Label sx={{ alignItems: "center" }}>
-                <Radio
-                  name="closure"
-                  value="roll-top"
-                  checked={this.state.closure == "roll-top"}
-                  onChange={this.handleStringChange}
-                />
-                Roll-top
-              </Label>
-              <Label sx={{ alignItems: "center" }}>
-                <Radio
-                  name="closure"
-                  value="draw-string"
-                  checked={this.state.closure == "draw-string"}
-                  onChange={this.handleStringChange}
-                />
-                Draw-string
-              </Label>
-            </Flex>
-            <Label>Fold</Label>
-            <Flex mb={3}>
-              <Label sx={{ alignItems: "center" }}>
-                <Radio
-                  name="fold"
-                  value="vertical"
-                  checked={this.state.fold == "vertical"}
-                  onChange={this.handleStringChange}
-                />
-                Vertical
-              </Label>
-              <Label sx={{ alignItems: "center" }}>
-                <Radio
-                  name="fold"
-                  value="horizontal"
-                  checked={this.state.fold == "horizontal"}
-                  onChange={this.handleStringChange}
-                />
-                Horizontal
-              </Label>
-            </Flex>
-            <Label>Bag Width: {this.state.sewWidth}</Label>
-            <Slider
-              sx={{
-                marginBottom: 3,
-              }}
-              name="sewWidth"
-              max="500"
-              min="0"
-              step="10"
-              value={this.state.sewWidth}
-              onChange={this.handleNumberChange}
-            />
-            <Label>Bag Height: {this.state.sewHeight}</Label>
-            <Slider
-              sx={{
-                marginBottom: 3,
-              }}
-              name="sewHeight"
-              max="500"
-              min="0"
-              step="10"
-              value={this.state.sewHeight}
-              onChange={this.handleNumberChange}
-            />
-            <Label>Seam Allowance: {this.state.allowance}</Label>
-            <Slider
-              sx={{
-                marginBottom: 3,
-              }}
-              name="allowance"
-              max="50"
-              min="10"
-              step="5"
-              value={this.state.allowance}
-              onChange={this.handleNumberChange}
-            />
-            <Label>Webbing Width: {this.state.webbing}</Label>
-            <Slider
-              sx={{
-                marginBottom: 4,
-                "&:disabled": {
-                  color: "disabled",
-                  backgroundColor: "disabled",
-                },
-              }}
-              name="webbing"
-              max="50"
-              min="10"
-              step="5"
-              value={this.state.webbing}
-              onChange={this.handleNumberChange}
-              disabled={this.state.closure != "roll-top"}
-            />
+            <Box mb={4}>
+              <Label>Pattern</Label>
+              <Select onChange={this.handleSelectChange}>
+                <option value="roll-top,vertical">
+                  Roll-top Bag (vertical)
+                </option>
+                <option value="roll-top,horizontal">
+                  Roll-top Bag (horizontal)
+                </option>
+                <option value="draw-string,vertical">
+                  Draw-string Bag (vertical)
+                </option>
+                <option value="draw-string,horizontal">
+                  Draw-string Bag (horizontal)
+                </option>
+                <option value="roll-top,vertical">Zipper Sleeve</option>
+                <option value="roll-top,vertical">Zipper Cube</option>
+              </Select>
+            </Box>
+            <Box mb={4}>
+              <Label>Bag Width: {this.state.sewWidth}</Label>
+              <Slider
+                mb={3}
+                name="sewWidth"
+                max="500"
+                min="0"
+                step="10"
+                value={this.state.sewWidth}
+                onChange={this.handleNumberChange}
+              />
+              <Label>Bag Height: {this.state.sewHeight}</Label>
+              <Slider
+                mb={3}
+                name="sewHeight"
+                max="500"
+                min="0"
+                step="10"
+                value={this.state.sewHeight}
+                onChange={this.handleNumberChange}
+              />
+              <Label>Seam Allowance: {this.state.allowance}</Label>
+              <Slider
+                mb={3}
+                name="allowance"
+                max="50"
+                min="10"
+                step="5"
+                value={this.state.allowance}
+                onChange={this.handleNumberChange}
+              />
+              <Label>Webbing Width: {this.state.webbing}</Label>
+              <Slider
+                sx={{
+                  "&:disabled": {
+                    color: "disabled",
+                    backgroundColor: "disabled",
+                  },
+                }}
+                name="webbing"
+                max="50"
+                min="10"
+                step="5"
+                value={this.state.webbing}
+                onChange={this.handleNumberChange}
+                disabled={this.state.closure != "roll-top"}
+              />
+            </Box>
             <Button
               sx={{ width: "100%", marginBottom: 2 }}
               variant="primary"

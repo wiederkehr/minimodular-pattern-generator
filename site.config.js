d702802c963f26693555a925b8b6603dbf871237
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2021-02-07 18:56:33 -0500

    Add site config file

---
+++ site.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  url: "https://generator.minimodular.systems/",
+  title: "Minimodular Pattern Generator",
+  description:
+    "Sewing pattern generator for ultralight, minimal, modular gear for the outdoors.",
+  author: "Benjamin Wiederkehr",
+  authorURL: "https://benjaminwiederkehr.com",
+  analytics: "", // put your google analytics code here to add tracking
+  image: "/minimodular-share-image.png",
+};

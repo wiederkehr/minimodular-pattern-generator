83869cbc13a8cdb3e9e944826b27302680ce1669
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2021-02-08 20:49:45 -0500

    Add Layout component from minimodular website

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+/** @jsxImportSource theme-ui */
+import React from "react";
+
+const Layout: React.FC = ({ children }) => (
+  <div
+    sx={{
+      backgroundColor: "background",
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh",
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default Layout;
